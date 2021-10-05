5d5e955ed270290d1b7f5009ed9b2398e49d4dd5
bedeepmk <91084675+bedeepmk@users.noreply.github.com>
2021-10-06 03:35:48 +0900

    페이지추가 라우트수정

--- resources/views/layouts/client/header.blade.php
+++ resources/views/layouts/client/header.blade.php
@@ -1,15 +1,21 @@
 <div>
 @if (Route::has('login'))
     <div>
+            <a href="{{ url('/')  }}" >home</a>
+            <a href="{{ route('intro') }}">회사소개</a>
+            <a href="{{ route('whyus') }}">왜 CHIA 마이닝랩인가?</a>
+            <a href="{{ route('miningsite') }}">채굴현장</a>
+            <a href="{{ route('purchase') }}">채굴기구매</a>
+            <a href="{{ route('faq') }}">FAQ</a>
+            <a href="{{ route('contact') }}">문의하기</a>
         @auth
-            <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">home</a>
-            <a href="{{  route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">로그아웃</a>
+            <a href="{{  route('mypage.dashboard') }}">마이페이지</a>
+            <a href="{{  route('logout') }}">로그아웃</a>
         @else
-            <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">home</a>
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">로그인</a>
+            <a href="{{ route('login') }}">로그인</a>
 
             @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">회원가입</a>
+                <a href="{{ route('register') }}">회원가입</a>
             @endif
         @endauth
     </div>
